13bd5c15bd00aa66a023fba816279bd1c145ca92
den <den@teormeh.com>
2016-08-31 14:25:50 +0300

    Добавил заготовки арксинуса и арккосинуса

---
+++ asincos.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <math.h>
+
+#include "asincos.h"
+#include "common.h"
+#include "summ.h"
+
+
+void eq_calculate_asin(struct eq_node **node)
+{
+  
+}
+
+void eq_transform_asin(void **node)
+{
+  
+}
+
+void eq_calculate_acos(struct eq_node **node)
+{
+  
+}
+
+
+void eq_transform_acos(void **node)
+{
+  
+}
---
+++ asincos.h
@@ -0,0 +1,11 @@
+#ifndef ASINCOS_H
+#define ASINCOS_H
+
+#include "eqtypes.h"
+
+void eq_calculate_asin(struct eq_node **node);
+void eq_transform_asin(void **node);
+void eq_calculate_acos(struct eq_node **node);
+void eq_transform_acos(void **node);
+
+#endif /*ASINCOS_H*/
