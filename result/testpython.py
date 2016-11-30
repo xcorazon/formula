d217a5e82e69d9db5bb012d6d1c23dfd76f963ad
xcorazon <corazon-v@mail.ru>
2016-11-30 17:23:49 +0300

    Изменил тестовые файлы.

--- result/testpython.py
+++ result/testpython.py
@@ -8,9 +8,12 @@ print a.toStarMath()
 
 b=formula.sin('%ialpha')
 print b.toStarMath()
-c=(formula.cos(b) + "a_1"+"c_2"-5.1)
+c=formula.cos(b) / (formula.symbol("a_1")+"c_2"-5.1)
 c /= b + 3.5
 
 print c.toStarMath()
+
+d = formula.symbol("a") / (formula.number(2)/"b")
+print d.toStarMath()
 
 print "EQ_COS = %d" % formula.EQ_COS
--- test.c
+++ test.c
@@ -13,98 +13,56 @@
 
 int main(int argc, char *argv[])
 {
-  //struct eq_node *head = eq_node_new(EQ_SUMM, 1);
-  struct eq_node *summ = eq_node_new(EQ_SUMM, 1);
-  struct eq_node *mul  = eq_node_new(EQ_MUL, 1);
-  struct eq_node *rec  = eq_node_new(EQ_RECIPROCAL, 1);
-  //struct eq_node *sinus  = eq_node_new(EQ_SIN, 1);
-  //struct eq_node *asinus  = eq_node_new(EQ_ASIN, -1);
-  struct eq_leaf *a = eq_leaf_new(EQ_SYMBOL, 1, L"a", 0);
-  struct eq_leaf *b = eq_leaf_new(EQ_NUMBER, 1, L"b", 180);
-  struct eq_leaf *c = eq_leaf_new(EQ_SYMBOL, -1, L"a", 0);
-  struct eq_leaf *b1 = eq_leaf_new(EQ_SYMBOL, 1, L"b_1", 20.345);
-
-  setlocale(LC_ALL, "en_US.utf8");
-  
-  wprintf(L"cos(0.001) = %f, round = %i \n", cos(0.001), (int)round(cos(0.001)));
-  
-  wprintf(L"Size of wchar_t = %d\n", sizeof(wchar_t));
-  
-  rec->first_child = b1;
-  c->next = rec;
-  mul->first_child = c;
-  
-  summ->first_child = a;
-  a->next = mul;
-  mul->next = b;
-    
   wchar_t starmath_result[1000];
   wchar_t treeview_result[1000];
   
-  *starmath_result = 0;
-  sm_summ(summ, SM_ROUND_BRACKET, starmath_result);
-  wprintf(starmath_result);
+  //struct eq_node *head = eq_node_new(EQ_SUMM, 1);
+  struct eq_leaf *a1 = eq_leaf_new(EQ_SYMBOL, 1, L"a_1", 0);
+  struct eq_leaf *c2 = eq_leaf_new(EQ_SYMBOL, 1, L"c_2", 20.345);
+  struct eq_leaf *num = eq_leaf_new(EQ_NUMBER, -1, NULL, 5.1);
+  
+  struct eq_node *summ = eq_node_new(EQ_SUMM, 1);
+  
+  a1->next = c2;
+  c2->next = num;
+  summ->first_child = a1;
+  
+  struct eq_leaf *b = eq_leaf_new(EQ_SYMBOL, 1, L"b", 0);
+  struct eq_node *mul  = eq_node_new(EQ_MUL, 1);
+  struct eq_node *rec  = eq_node_new(EQ_RECIPROCAL, 1);
+  rec->first_child = summ;
+  mul->first_child = b;
+  b->next = rec;
+  
+  eq_transform(&mul);
+  
+  struct eq_node *mul2  = eq_node_new(EQ_MUL, 1);
+  struct eq_node *rec2  = eq_node_new(EQ_RECIPROCAL, 1);
+  mul2->first_child = mul;
+  mul->next = rec2;
+  
+  rec2->first_child = eq_leaf_new(EQ_SYMBOL, 1, L"b_2", 0);
   
   *treeview_result = 0;
-  tree_view(summ, SM_ROUND_BRACKET, treeview_result);
-  wprintf(treeview_result);
+  tree_view(mul2, 4, treeview_result);
+  debug(L"treeview 1 \n %s", treeview_result);
   
-  eq_calculate((void**)&summ);
+  eq_transform(&mul2);
+  //eq_transform(&mul2);
   
   *treeview_result = 0;
-  tree_view(summ, 0, treeview_result);
-  debug(treeview_result);
-  
-  wprintf(L"\ntesting %f\n", 20.1);
-  
-  wprintf(L"\nresult: %s\n", treeview_result);
-  
-  eq_transform((void**)&summ);
-  wprintf(L"\nhead transformed...\n");  
-  
-  eq_combine_summ(summ);
-  eq_calculate_summ(&summ);
+  tree_view(mul2, 4, treeview_result);
+  debug(L"treeview 2 \n%s", treeview_result);
   
   *starmath_result = 0;
-  sm_summ(summ, 0, starmath_result);
-  debug(starmath_result);
-
-  struct eq_node *multiplier = eq_find_common_mul(summ);
-  wprintf(L"\nfinded common multiplier...\n");
+  sm_to_string(mul2, SM_DEFAULT, starmath_result);
+  debug(L"%s",starmath_result);
   
-  eq_move_multipliers_out(&summ);
-  wprintf(L"\nmultiplier moved out...\n");
-
   *treeview_result = 0;
-  tree_view(summ, 0, treeview_result);
-  wprintf(L"\nAfter move multiplier\n%s", treeview_result);
+  tree_view(mul2, 4, treeview_result);
+  debug(L"%s", treeview_result);
   
-  eq_calculate((void**)&summ);
-  eq_calculate((void**)&summ);
-  
-  tree_view(summ, 0, treeview_result);
-  wprintf(treeview_result);
-  
-  *starmath_result = 0;
-  sm_summ(summ, 0, starmath_result);
-  debug(starmath_result);
-  
-  eq_transform((void**)&summ);
-  debug(L"После eq_transform.");
-  *starmath_result = 0;
-  sm_summ(summ, 0, starmath_result);
-  debug(starmath_result);
-  
-  eq_transform((void**)&summ);
-  
-  struct eq_leaf *number = get_summ_number(summ);
-  if(number != NULL)
-    wprintf(L"\nnumber = %f\n", number->value);
-  
-  *starmath_result = 0;
-  sm_summ(summ, 0, starmath_result);
-  debug(starmath_result);
-  eq_node_delete(summ);
-  
+  eq_delete(mul2);
+    
   return 0;
 }
